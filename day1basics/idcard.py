89a85781eb3d99886d4a2c35bf72ffca7c5a2e38
naman yadav <naman18gaming@gmail.com>
2026-05-27 02:08:21 +0530

    Day 1: Student ID Card program in Python

---
+++ day1basics/idcard.py
@@ -0,0 +1,11 @@
+# student id card
+name = input("enter your name:")
+
+age = int(input("enter your age:"))
+
+school = input("enter your school name:")
+
+print("---- STUDENT ID CARD----")
+print("Name:", name)
+print("Age:", age)
+print("School:", school)
