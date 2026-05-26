8b5414ecac8b79d8799b4a4dc4ab7fd0f1664ee1
naman yadav <naman18gaming@gmail.com>
2026-05-27 01:36:53 +0530

    Day 1: Minutes to Hours conversion program in Python

---
+++ day1basics/mintohour.py
@@ -0,0 +1,10 @@
+# Minutes to Hours Conversion
+
+# Take input from user
+minutes = float(input("Enter the value of minutes: "))
+
+# Convert minutes to hours
+hours = minutes / 60
+
+# Print result
+print("The value of hours is:", hours)
