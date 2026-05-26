6a5f97e73638bdf2c1e0706519b41d041ee39ff1
naman yadav <naman18gaming@gmail.com>
2026-05-27 02:22:57 +0530

    Day 1: Calculator program in Python

---
+++ day1basics/calculator.py
@@ -0,0 +1,8 @@
+#calculator 
+x = int(input("eneter the value of x:"))
+b = int(input("eneter the value of b:"))
+print("the sum of x and b is :", x+b)
+print("the difference of x and b is :", x-b)
+print("the sum of x and b is :", x+b)
+print("the product of x and b is :", x*b)
+print("the quotient of x and b is :", x/b)
