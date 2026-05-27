845259b7417a4e8d3f849fa3bfcfca2b2ba189c1
naman yadav <naman18gaming@gmail.com>
2026-05-27 13:33:56 +0530

    Day 1: Square and Cube program in Python

---
+++ day1basics/sqaurecube.py
@@ -0,0 +1,12 @@
+# Find square and cube of a number
+
+# Take input from user
+num = int(input("Enter the number: "))
+
+# Calculate square and cube
+square = num ** 2
+cube = num ** 3
+
+# Print results
+print("The square of the number is:", square)
+print("The cube of the number is:", cube)
