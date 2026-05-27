057504a9794a4b01f312a4ee13027759d09ff82c
naman yadav <naman18gaming@gmail.com>
2026-05-27 16:33:00 +0530

    Day 1: Average calculation program in Python

---
+++ day1basics/average%.py
@@ -0,0 +1,9 @@
+# average of 5 subject percentage
+s1 = int(input("enter the marks of the subject 1 :"))
+s2 = int(input("enter the marks of the subject 2 :"))
+s3 = int(input("enter the marks of the subject 3 :"))
+s4 = int(input("enter the marks of the subject 4 :"))
+s5 = int(input("enter the marks of the subject 5 :"))
+total = s1 + s2 + s3 + s4 + s5
+average =  total / 5
+print("the average of 5 subject is :", average)
