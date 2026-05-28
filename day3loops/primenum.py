27d054676ac40b485b2b88b4c3f13b1578c2340d
naman yadav <naman18gaming@gmail.com>
2026-05-28 15:05:51 +0530

    Day 3: Prime number program in Python

---
+++ day3loops/primenum.py
@@ -0,0 +1,13 @@
+# print prime numbers from 1 to 100
+print("prime numbers from 1 to 100")
+
+for num in range(1, 101):
+    if num <= 1:
+        continue
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
