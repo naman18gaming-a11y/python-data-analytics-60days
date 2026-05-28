e8b05e714a9e0469fb6b1f4da686df67a51c0411
naman yadav <naman18gaming@gmail.com>
2026-05-29 01:37:11 +0530

    Day 4: Program to count words in a string

---
+++ day4string/countword.py
@@ -0,0 +1,4 @@
+# Count Words in Sentence
+str = input("enter the string :")
+print("the number of words in the string is:", len(str.split()))
+print("the number of characters in the string is:", len(str))
