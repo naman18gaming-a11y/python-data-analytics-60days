94aaf5bd6e2f3f750d1fc5651359c1e9c2f28553
naman yadav <naman18gaming@gmail.com>
2026-05-31 03:07:30 +0530

    Day 6: Find maximum and minimum in list program

---
+++ day6lists/maxminlist.py
@@ -0,0 +1,6 @@
+# max and min of a list
+numbers = [1, 2, 3, 4, 5]
+max_num = max(numbers)
+min_num = min(numbers)
+print("The maximum number in the list is:", max_num)
+print("The minimum number in the list is:", min_num)
