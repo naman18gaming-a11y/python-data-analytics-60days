cfb33789956959976dae398fa73c6085c6c7bb73
naman yadav <naman18gaming@gmail.com>
2026-05-31 02:49:48 +0530

    Day 6: Ascending and descending sort program

---
+++ day6lists/asenddecend.py
@@ -0,0 +1,8 @@
+# ascending and descending order of a list
+numbers = [5, 2, 9, 1, 5, 6]
+# Ascending order
+numbers.sort()
+print("Ascending order:", numbers)
+# Descending order
+numbers.sort(reverse=True)
+print("Descending order:", numbers) 
