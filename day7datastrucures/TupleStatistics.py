cff672a66b70737ec1108eff52e393a4bbecbb23
naman yadav <naman18gaming@gmail.com>
2026-05-31 16:35:10 +0530

    Day 7: Tuple statistics program

---
+++ day7datastrucures/TupleStatistics.py
@@ -0,0 +1,11 @@
+# Tuple Statistics
+marks = (78, 85, 90, 67, 92)
+max_marks = max(marks)
+print(max_marks)
+min_marks = min(marks)
+print(min_marks)
+total_marks = sum(marks)
+print(total_marks)
+lenght = len(marks)
+average = total_marks / lenght
+print(average)
